c73154d8ed6ed9a8efbdd4c6b8afd7f7622db9c1
Thaís Hübner <izzyhubner@gmail.com>
2021-01-25 01:43:16 -0300

    DP6

---
+++ js/tagueamento.js
@@ -0,0 +1,24 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+/*function handleOutboundLinkClicks(event_contato) {
+    ga('send', 'event', {
+        eventCategory: 'menu',
+        eventAction: 'entre_em_contato',
+        eventLabel: event_contato.target.href,
+        transport: 'beacon'
+    });
+}
+
+function handleOutboundLinkClicks(event) {
+    ga('send', 'event', {
+        eventCategory: 'menu',
+        eventAction: 'download_pdf',
+        eventLabel: event.target.href,
+        transport: 'beacon'
+    });
+}*/
